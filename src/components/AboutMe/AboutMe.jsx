f1d26309cd0eb5dc55f1d73c5e0323d0ed81be51
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-02 10:55:54 +0200

    feat: add AboutMe component

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,29 @@
+function AboutMe() {
+
+    const aboutMeTex= `I'm Daniel Ciciliani and I started creating websites back in 2019, just out of curiosity. 
+    During the pandemic in 2020, I began selling my websites as a freelancer, and that's when 
+    I made my first bucks "having fun". I realized I could actually make a living just by coding, and without even noticing 
+    I made a career switch. I was doubling my monthly income, so it felt the right time to go 
+    all in on web development.
+    
+    In 2021 I moved to Spain and started working as a Frontend Developer in advertising, building websites for end clients. 
+    The following year, I joined Grupo Juinsa, where I grew as
+    a Frontend Developer, working both frontend and backend for two years.
+
+    Recently, I have been building personal projects and sharpening my skills to
+    take the next step in my career. I am looking to join a growth-oriented company, where 
+    vision and strategy are aligned with the product. A company where I can contribute not just with my technical skills 
+    but also through solid communication, collaboration, and problem-solving skills.
+    `;
+
+    return (
+        <>
+            <div className="flex flex-col gap-10">
+                <h2 className="text-4xl">About me</h2>
+                <p>{aboutMeTex}</p>
+            </div>
+        </>
+    );
+};
+
+export default AboutMe;
