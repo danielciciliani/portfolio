a49e60cdde625532bc0f1d888c6daa52dfb781a4
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-02 11:24:07 +0200

    fix: fix image route from public

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,14 @@
-import profilePicture from '/profile_picture_1.png'
 
 function Profile(){
+
+    const profilePicture = "/profile_picture_1.png";
 
     return (
         <>
             <div className="flex flex-col items-center">
                 <div className="w-[100%] flex justify-center py-10">
                     <div className="w-25 h-25 rounded-full">
-                        <img className='rounded-full h-[100%] w-[100%] object-cover object-top' src={profilePicture} alt="" />
+                        <img className='rounded-full h-[100%] w-[100%] object-cover object-top' src={profilePicture} alt="Profile picture" />
                     </div>
                 </div>
                 <h1>Hey, I'm <span className='bg-gradient-to-r from-pink-500 to-blue-500 bg-clip-text text-transparent pt-10'>Daniel Ciciliani</span></h1>
