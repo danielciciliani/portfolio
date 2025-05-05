3c6f660e73f362e79b9e295d615707d0135540c2
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-05 13:45:53 +0200

    chore: add new project

--- src/utils/ProjectsData.jsx
+++ src/utils/ProjectsData.jsx
@@ -3,5 +3,6 @@ export const ProjectsData = [
     {id: 1, name: "MBST Mobile Store", tech: ['React', 'Sass', 'Jest'], details: `Developed websites using visual builders, customizing base templates`, buttons: { code: "link", preview: ""} , image: 'projects/mbst_1.png'},
     {id: 2, name: "Cinema Production", tech: ['Javascript', 'GSAP', 'Swiper.js'], details: `Developed websites using visual builders, customizing base templates`, buttons: { code: "link", preview: ""}, image: 'projects/yoro_1.png'},
     {id: 3, name: "B2B E-commerce", tech: ['React', 'Tailwind'], details: `Developed websites using visual builders, customizing base templates`, buttons: { code: "link", preview: ""}, image: 'projects/ix_1.png'},
-    {id: 4, name: "Consultant", tech: ['HTML', 'CSS', 'Javascript'], details: `Developed websites using visual builders, customizing base templates`, buttons: { code: "link", preview: ""}, image: 'projects/pci_1.png'}    
+    {id: 4, name: "Personal Website", tech: ['React', 'Tailwind'], details: `Developed websites using visual builders, customizing base templates`, buttons: { code: "link", preview: ""}, image: 'projects/dcp_1.png'},
+    {id: 5, name: "Consultant", tech: ['HTML', 'CSS', 'Javascript'], details: `Developed websites using visual builders, customizing base templates`, buttons: { code: "link", preview: ""}, image: 'projects/pci_1.png'}    
 ];
