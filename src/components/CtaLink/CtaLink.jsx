f3c1edbf94afc53c21b580052e9e052b533aed70
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-05 11:46:45 +0200

    styles: add styles to link sections on mobile

--- src/components/CtaLink/CtaLink.jsx
+++ src/components/CtaLink/CtaLink.jsx
@@ -1,22 +1,27 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaCloudDownloadAlt } from "react-icons/fa";
 
 function CtaLink(){
 
 const links = [
     {id: 1, link: 'linkedin.com' , text: 'linkedin', icon: FaLinkedin},
-    {id: 2, link: 'github.com' , text: 'github', icon: FaGithub}
+    {id: 2, link: 'github.com' , text: 'github', icon: FaGithub},
+    {id: 3, link: 'github.com' , text:  'CV', icon: FaCloudDownloadAlt}
 ];
 
 
     return (
         <>
-            <div className="flex gap-4 content-between w-[100%] justify-center">
+            <div className="flex gap-2 md:gap-4 content-between w-full justify-center">
             {
                 
                 links.map( (e) => {
                     const Icon = e.icon;
                     return ( 
-                    <div key={e.id} className="border-1  border-blue-500 rounded-2xl px-5 py-1 text-color-base min-w-[150px] hover:border-pink-500 hover:text-white hover:scale-110 transition-all duration-200">
+                    <div key={e.id} className="
+                    text-sm md:text-xl border-1  border-blue-500 rounded-2xl 
+                    px-5 py-1 text-color-base min-w-[30%] md:min-w-[150px]
+                    hover:border-pink-500 hover:text-white hover:scale-110
+                    transition-all duration-200">
                         <a 
                             href={e.link}
                             target="_blank"
--- src/components/Panda/Panda.jsx
+++ src/components/Panda/Panda.jsx
@@ -3,13 +3,13 @@ function Panda() {
     return (
         <>
             <div className="w-full flex flex-row justify-center">
-                <div className="w-40 h-40">
+                <div className="w-30 h-30 md:w-40 md:h-40">
                     <img className="w-full h-full" src="/panda_transparent_logo_l.png" alt="red panda logo" />    
                 </div>
-                <div className="w-40 h-40">
+                <div className="w-30 h-30 md:w-40 md:h-40">
                     <img className="w-full h-full" src="/panda_transparent_logo.png" alt="red panda logo" />    
                 </div>
-                <div className="w-40 h-40">
+                <div className="w-30 h-30 md:w-40 md:h-40">
                     <img className="w-full h-full" src="/panda_transparent_logo_r.png" alt="red panda logo" />    
                 </div>
             </div>  
