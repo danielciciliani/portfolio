30232dcba9549add70b0a3fe4c420b90a733ddd9
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-06 11:35:33 +0200

    refactor: move Scroll animation to a utility component

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,14 @@ import Experience from "./components/Experience/Experience";
 import Projects from "./components/Projects/Projects";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Panda from "./components/Panda/Panda";
-import {motion, useScroll} from "motion/react";
 import "./index.css";
+import Scroll from "./utils/Animations/Scroll";
 
 function App() {
 
-  const {scrollYProgress} = useScroll();
-
   return (
     <>
-      <motion.div
-        id="scroll-indicator"
-        className="bg-main-gradient"
-        style={{
-          scaleX: scrollYProgress,
-          position: "fixed",
-          top: 0,
-          left: 0, 
-          right: 0,
-          height: 2,
-          originX: 0,
-        }}
-      />
+      <Scroll></Scroll>
         <div className="font-lato w-screen h-full">
         <div className="w-11/12 mx-auto sm:w-9/12 lg:w-7/12">
           <div className="flex flex-col gap-10 pt-10">
@@ -48,9 +34,6 @@ function App() {
           </div>
         </div>
       </div>
-
-      
-      
     </>
   );
 }
---
+++ src/utils/Animations/Scroll.jsx
@@ -0,0 +1,29 @@
+import { motion, useScroll, useSpring } from 'motion/react';
+
+function Scroll(){
+    
+    const {scrollYProgress} = useScroll();
+    
+    return (
+        <motion.div
+            id="scroll-indicator"
+            className="bg-main-gradient"
+            style={{
+            scaleX: useSpring(scrollYProgress, {
+                stiffness: 100, 
+                damping: 30,
+                restDelta: 0.001,
+            }),
+            position: "fixed",
+            top: 0,
+            left: 0, 
+            right: 0,
+            height: 2,
+            originX: 0,
+            zIndex: 50,
+            }}
+        />
+    );
+}
+
+export default Scroll;
