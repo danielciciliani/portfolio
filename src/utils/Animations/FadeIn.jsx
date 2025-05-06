b7509cb1922aac7226424a585a70afb1af97bd23
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-06 14:01:21 +0200

    fix: change amount viewport in animation

--- src/utils/Animations/FadeIn.jsx
+++ src/utils/Animations/FadeIn.jsx
@@ -6,7 +6,7 @@ function FadeIn({children, className="", delay=0, duration=1.5}) {
             className={className}
             initial={{ opacity: 0, scale: 0 }}
             whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true, margin: "0px 0px -200px 0px" }}
+            viewport={{ once: true, amount: 0 }}
             onViewportEnter={(entry) => console.log(entry.isIntersecting)}
             transition={{
                 duration,
