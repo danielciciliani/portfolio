5d914f04a28b10724dba42f4b8b75fda001c3e00
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-07 13:07:21 +0200

    refactor: move experience data to utils

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,76 +1,9 @@
 import FadeInFromBottom from "../../utils/Animations/FadeInFromBottom";
+import { ExperienceData } from "../../utils/ExperienceData";
+
 
 function Experience() {
-  const experiences = [
-    {
-      id: 1,
-      jobTitle: "Frontend Developer",
-      company: "Grupo Juinsa",
-      from: "07/2022 - 07/2024",
-      details: [
-        <>
-          Developed a real-time dashboard using React and Javascript to monitor
-          deployments in Staging and Production environments, integrating the
-          Bitbucket API to fetch and display status.
-        </>,
-        <>
-          Integrated Conset Managemenet Service with Google Tag Manager services
-          to track cookie consents in the project.
-        </>,
-        <>
-          Implemented new themes in two of the group's stores, reflecting the
-          company’s new visual identity.
-        </>,
-        <>
-          Developed, maintained, and modified custom modules. And adapted
-          functionalities in proprietary Magento modules.
-        </>,
-        <>
-          Migrated custom modules to Composer, improving version management in
-          Git.
-        </>,
-        <>
-          Led the support panel, debugging and bug fixing both Frontend and
-          Backend tickets.
-        </>,
-        <>
-          Optimized or reduce SQL queries, improving the general performance and
-          quality of the code.
-        </>,
-        <>
-          Delivered demo presentations, to showcase the new features to the
-          interested parts.
-        </>,
-      ],
-    },
-    {
-      id: 2,
-      jobTitle: "Frontend Developer",
-      company: "Modern Talking",
-      from: "01/2022 - 06/2022",
-      details: [
-        <>
-          Developed custom websites from scatch for end-clients, implementing
-          designs provided in Figma by the Design Team.
-        </>,
-        <>
-          Designed wireframes based on client needs, proposing functional and
-          modern alternatives to deliver to the UI team.
-        </>,
-      ],
-    },
-    {
-      id: 3,
-      jobTitle: "Frontend Developer",
-      company: "Van Agency",
-      from: "09/2021 - 12/2021",
-      details: [
-        <>
-          Developed websites using visual builders, customizing base templates.
-        </>,
-      ],
-    },
-  ];
+  const experiences = ExperienceData;
 
   return (
     <>
