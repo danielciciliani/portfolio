fdeaa64eed8444dc0b69bed00be5efb63c57e79f
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-07 17:35:11 +0200

    chore: set once parameter to animation

--- src/utils/Animations/FadeIn.jsx
+++ src/utils/Animations/FadeIn.jsx
@@ -1,12 +1,12 @@
 import { motion } from "motion/react";
 
-function FadeIn({ children, className = "", delay = 0, duration = 1.5 }) {
+function FadeIn({ children, className = "", delay = 0, duration = 1.5, once=true}) {
   return (
     <motion.div
       className={className}
       initial={{ opacity: 0, scale: 0 }}
       whileInView={{ opacity: 1, scale: 1 }}
-      viewport={{ once: true, amount: 0 }}
+      viewport={{ once, amount: 0 }}
       transition={{
         duration,
         delay,
