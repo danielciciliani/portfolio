132b713f0238457ce05421ba894b535b4a710af9
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-07 18:44:56 +0200

    chore: add text to Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,8 @@ function Contact({children}){
     return (
         <>
             <div className="flex flex-col cursor-pointer w-full">
-                <h2 className="text-5xl font-extrabold">Do we talk?</h2>
+                <h2 className="text-5xl font-extrabold">Wanna chat?</h2>
+                <p className="pt-10 pb-5">If you think I could be a good fit for your team, or maybe you wanna build a project together, let's chat!</p>
                 <FadeIn delay={1.5} className="pt-5 flex self-end" >
                     {children}
                 </FadeIn>
