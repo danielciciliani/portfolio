84b095735cb1c0364fac144e54742b2d16ce0a49
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-08 17:04:29 +0200

    styles: modify grandient styles on cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ import Profile from "../Profile/Profile";
 function Card({ children }) {
   return (
     <>
-      <div className="flex border-gradient rounded-4xl p-10">
+      <div className="flex border-gradient rounded-4xl p-10 shadow-[0_0_20px_1px_rgba(255,255,255,0.1)]">
         {children}
       </div>
     </>
