5c3c30d7b8b1db031d886b1f7c9db4badc4edb0d
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-13 12:24:40 +0200

    chore: improve data in projects and refactor data and icons utilities

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import { motion } from "motion/react";
 import FadeInFromBottom from "../../utils/Animations/FadeInFromBottom";
 import { ProjectsData } from "../../utils/ProjectsData";
+import { div } from "motion/react-client";
 
 function Projects() {
   const projects = ProjectsData;
@@ -16,10 +17,14 @@ function Projects() {
           key={project.id}
           className="w-full h-auto flex flex-col lg:flex-row  gap-5 pb-10 last:pb-0"
         >
-          <div className="w-full lg:w-[70%] rounded-xl border-[.5] border-gradient overflow-hidden">
+          <div className="w-full lg:min-w-[60%] lg:max-w-[60%] rounded-xl border-[.5] border-gradient overflow-hidden">
             <motion.img
               whileHover={{ scale: 1.2 }}
-              className="rounded-xl"
+              transition={{
+                stiffness: 300,
+                damping: 15,
+              }}
+              className="rounded-xl w-full h-full contain-content"
               src={project.image}
               alt={project.name}
             />
@@ -28,20 +33,41 @@ function Projects() {
             <h3 className="text-2xl md:text-4xl font-extrabold pt-1">
               {project.name}
             </h3>
-            <div className="flex gap-1.5 pt-2 pb-5">
-              {project.tech.map((techItem, index) => (
+            <div className="flex flex-wrap gap-1.5 pt-2 pb-5 text-white">
+              {project.tech?.map((item, index) => (
                 <div
-                  className="cursor-pointer text-sm border-1  border-slate-500 rounded-2xl px-5 min-w-4 py-1 text-center bg-slate-500"
+                  className="flex items-center gap-1 text-sm border-1 border-blue-500/90 dark:border-slate-700 rounded-2xl px-3 min-w-4 py-1 text-center bg-blue-900/60 dark:bg-blue-500/10"
                   key={index}
                 >
-                  {techItem}
+                  {item.icon}
+                  {item.name}
                 </div>
               ))}
             </div>
             <div className="detail">{project.details}</div>
-            <div>
-              <div className="code">{project.buttons.code}</div>
-              <div className="preview">{project.buttons.preview}</div>
+            <div className="flex pt-5 gap-3">
+              {project.buttons.map((button, index) => (
+                <motion.div key={index} 
+                className="
+                border-1 py-1 px-5 rounded-2xl text-sm sm:text-md 2xl:text-xl border-blue-500
+                text-color-base md:min-w-[100px] hover:border-pink-500
+                cursor-pointer
+                
+                "
+                whileHover={{
+                  scale: 1.05,
+                  boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.15)",
+                }}
+                whileTap={{ scale: 0.95 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 300,
+                  damping: 15,
+                }}
+                >
+                  <a href={button.link} target="_blank" className="flex items-center justify-center gap-2">{button.icon}{button.name}</a>
+                </motion.div>
+              ))}
             </div>
           </div>
         </FadeInFromBottom>
--- src/utils/ProjectsData.jsx
+++ src/utils/ProjectsData.jsx
@@ -1,42 +1,139 @@
+import {
+  ReactLogo,
+  TailwindCSS,
+  ReactRouter,
+  MySQL,
+  Php,
+  Sass,
+  Jest,
+  HTML5,
+  JavaScript,
+  Docker,
+  Motion,
+  Vite,
+  Vitest,
+} from "./TechIcons"
+
+import {
+  FaGithub,
+  FaExternalLinkSquareAlt
+} from "react-icons/fa";
+
+
+const buttonIcons = {
+  code: <FaGithub size={20}></FaGithub>,
+  preview: <FaExternalLinkSquareAlt size={20}></FaExternalLinkSquareAlt>
+}
+
+const tech = {
+  react: {
+    name: "React",
+    icon: <ReactLogo></ReactLogo>,
+  },
+  reactRouter: {
+    name: "React Router",
+    icon: <ReactRouter></ReactRouter>,
+  },
+  tailwind: {
+    name: "Tailwind",
+    icon: <TailwindCSS></TailwindCSS>,
+  },
+  sass: {
+    name: "Sass",
+    icon: <Sass></Sass>,
+  },
+  php: {
+    name: "PHP",
+    icon: <Php></Php>,
+  },
+  docker: {
+    name: "Docker",
+    icon: <Docker></Docker>,
+  },
+  mysql: {
+    name: "MySQL",
+    icon: <MySQL></MySQL>,
+  },
+  jest: {
+    name: "Jest",
+    icon: <Jest></Jest>,
+  },
+  motion: {
+    name: "Motion",
+    icon: <Motion></Motion>,
+  },
+  javascript: {
+    name: "Javascript",
+    icon: <JavaScript></JavaScript>,
+  },
+  html: {
+    name: "HTML",
+    icon: <HTML5></HTML5>,
+  },
+  vitest: {
+    name: "Vitest",
+    icon: <Vitest></Vitest>,
+  },
+  vite: {
+    name: "Vite",
+    icon: <Vite></Vite>,
+  },
+};
+
 export const ProjectsData = [
+
   {
     id: 1,
     name: "MBST Mobile Store",
-    tech: ["React", "Sass", "Jest"],
-    details: `Developed websites using visual builders, customizing base templates`,
-    buttons: { code: "link", preview: "" },
-    image: "projects/mbst_1.png",
+    tech: [tech.react, tech.tailwind, tech.vite, tech.reactRouter, tech.jest],
+    details: `Single Page Application (SPA), created following the requirements of a challenge: fetch data from API, filter, context, persistant data.`,
+    buttons: [
+      {
+        icon: buttonIcons.code,
+        name: "Code",
+        link: "https://github.com/danielciciliani/zara-challenge-dc",
+      },
+      { icon: buttonIcons.preview, name: "Preview", link: "https://zara-mbst-dc.vercel.app" },
+    ],
+    image: "projects/mbst_1.webp",
+    visible: true,
   },
   {
     id: 2,
     name: "Cinema Production",
-    tech: ["Javascript", "GSAP", "Swiper.js"],
-    details: `Developed websites using visual builders, customizing base templates`,
-    buttons: { code: "link", preview: "" },
-    image: "projects/yoro_1.png",
+    tech: [tech.javascript, tech.motion],
+    details: `Corportate website for cinema production company`,
+    buttons: [{ icon: buttonIcons.preview, name: "Preview", link: "https://www.yoromedia.com" }],
+    image: "projects/yoro_1.webp",
+    visible: true,
   },
   {
     id: 3,
     name: "B2B E-commerce",
-    tech: ["React", "Tailwind"],
-    details: `Developed websites using visual builders, customizing base templates`,
-    buttons: { code: "link", preview: "" },
-    image: "projects/ix_1.png",
+    tech: [tech.react, tech.javascript, tech.sass, tech.php, tech.docker],
+    details: `E-commerce with more than 60k products, and 10k clients.`,
+    buttons: [{ icon: buttonIcons.preview, name: "Preview", link: "https://ixia.es/" }],
+    image: "projects/ix_1.webp",
+    visible: true,
   },
   {
     id: 4,
     name: "Personal Website",
-    tech: ["React", "Tailwind"],
+    tech: [tech.react, tech.tailwind, tech.motion, tech.motion, tech.vitest],
     details: `Developed websites using visual builders, customizing base templates`,
-    buttons: { code: "link", preview: "" },
-    image: "projects/dcp_1.png",
+    buttons: [
+      { icon: buttonIcons.code, name: "Code", link: "https://github.com/danielciciliani/portfolio" },
+    ],
+    image: "projects/dcp_dark.webp",
+    visible: true,
   },
   {
     id: 5,
-    name: "Consultant",
-    tech: ["HTML", "CSS", "Javascript"],
-    details: `Developed websites using visual builders, customizing base templates`,
-    buttons: { code: "link", preview: "" },
-    image: "projects/pci_1.png",
+    name: "Coaching",
+    tech: [tech.javascript, tech.html, tech.sass, tech.php],
+    details: `Corporate website with integrated appointment scheduling`,
+    buttons: [{ icon: buttonIcons.preview, name: "Preview", link: "https://pleasecomein.co.uk" }],
+    image: "projects/pci_1.webp",
+    visible: true,
   },
 ];
