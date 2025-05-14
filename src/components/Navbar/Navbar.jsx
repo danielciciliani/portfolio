21c0e823a5f0a74fe93ffa44a89d50e301b002b8
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-14 12:11:01 +0200

    chore: set dark mode active by default

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,9 +26,8 @@ function Navbar() {
   const [darkMode, setDarkMode] = useState(() => {
     const stored = localStorage.getItem("dark mode");
   
-    if (stored !== null) return stored === "true";
+    return stored !== null ? stored === "true" : true;
     
-    return window.matchMedia("(prefers-color-scheme: dark)").matches;
   });
   
 
