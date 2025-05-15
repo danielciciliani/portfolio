c41446d7666d646843079ba6cc2a44e8cb32dadc
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-15 12:05:55 +0200

    chore: upload new style cv and delete previous one

--- src/utils/LinksData.jsx
+++ src/utils/LinksData.jsx
@@ -20,7 +20,7 @@ export const Links = [
   },
   {
     id: 3,
-    link: "./CV_DanielCiciliani_frontendReactDeveloper-EN.pdf",
+    link: "./CV_DanielCiciliani_FontendDeveloper2025.pdf",
     text: "CV",
     icon: FaCloudDownloadAlt,
     download: "CvDanielCiciliani",
