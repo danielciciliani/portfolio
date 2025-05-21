f53e39a5bc112957de55f16880bb6ee34897c549
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-05-21 12:09:48 +0200

    fix: set full width in contact card

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,10 +6,10 @@ import { mailTo } from "../../utils/LinksData";
 function Contact({ children }) {
   return (
     <>
-    <section id="contact" >
-      <div className="flex flex-col cursor-pointer w-full">
+    <section id="contact" className="w-full">
+      <div className="flex flex-col cursor-pointer">
         <h2 className="text-5xl font-extrabold">Let's talk!</h2>
-        <p className="pt-10 pb-5">
+        <p className="pt-10 pb-5 text-lg">
           If you think we can build something together,
           <span className="font-bold">
             <a href={mailTo}> let's get in touch!</a>
